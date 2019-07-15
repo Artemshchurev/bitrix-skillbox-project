1abd0fcc2fc05aa09446c69e5ff823643ce1254d
artemshchurev <artemshchurev@gmail.com>
2019-07-15 23:20:24 +1000

    Поменял title и заголовки

--- local/templates/shopselle/components/bitrix/catalog.section/main/result_modifier.php
+++ local/templates/shopselle/components/bitrix/catalog.section/main/result_modifier.php
@@ -7,6 +7,7 @@
 
 $component = $this->getComponent();
 $arParams = $component->applyTemplateModifications();
+// ГОРОД
 if (!empty($_COOKIE['CITY'])) {
     $arResult['IPROPERTY_VALUES']['SECTION_META_TITLE'] = str_replace(
         '#CITY#',
@@ -14,7 +15,38 @@ if (!empty($_COOKIE['CITY'])) {
         $arResult['IPROPERTY_VALUES']['SECTION_META_TITLE']
     );
 }
-
+// КОЛИЧЕСТВО
+$arFilter = ["IBLOCK_ID" => 2, "ACTIVE_DATE" => "Y", "ACTIVE" => "Y", "INCLUDE_SUBSECTIONS" => "Y"];
+if ($arResult['ORIGINAL_PARAMETERS']['SECTION_CODE']) {
+    $arFilter['SECTION_CODE'] = $arResult['ORIGINAL_PARAMETERS']['SECTION_CODE'];
+}
+$goodsCount = CIBlockElement::GetList(
+    Array(),
+    $arFilter,
+    [],
+    false,
+    ["ID", "IBLOCK_ID",]
+);
+$arResult['IPROPERTY_VALUES']['SECTION_META_TITLE'] = str_replace(
+    '#QUANTITY#',
+    $goodsCount,
+    $arResult['IPROPERTY_VALUES']['SECTION_META_TITLE']
+);
+// МИН МАКС ЦЕНЫ
+$prices = [];
+foreach ($arResult['ITEMS'] as $item) {
+    $prices[] = $item['MIN_PRICE']['VALUE'];
+}
+$arResult['IPROPERTY_VALUES']['SECTION_META_TITLE'] = str_replace(
+    '#MIN#',
+    min($prices),
+    $arResult['IPROPERTY_VALUES']['SECTION_META_TITLE']
+);
+$arResult['IPROPERTY_VALUES']['SECTION_META_TITLE'] = str_replace(
+    '#MAX#',
+    max($prices),
+    $arResult['IPROPERTY_VALUES']['SECTION_META_TITLE']
+);
 if (!$arResult['NAME']) {
     $res = CIBlock::GetByID( $arResult['IBLOCK_ID']);
     if ($ar_res = $res->GetNext()){
